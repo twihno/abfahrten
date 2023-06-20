59336f76f54c3e2aee6eccfa7d82f0a7651b3153
Thomas Schuster <twihno@gmail.com>
2023-06-21 01:36:42 +0200

    🚧 improvements to data fetching

--- frontend/src/components/common/departures.tsx
+++ frontend/src/components/common/departures.tsx
@@ -7,11 +7,15 @@ export default function Departures() {
   const settings = useSettings();
   const { data: departures, error, isLoading } = useDepartures(settings);
 
-  if (settings === undefined) {
+  if (settings === undefined || isLoading) {
     return <div>Loading...</div>;
   }
   if (settings === null) {
     return <div>Error: Invalid settings</div>;
+  }
+
+  if (error) {
+    return <div>Error in data fetching: {error.message}</div>;
   }
 
   console.log(departures);
