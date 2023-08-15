28362df67efeeb4dddd4b322325e4ce06ae78cc5
Thomas Schuster <twihno@gmail.com>
2023-08-15 22:23:48 +0200

    ⬆️ upgrade dependencies

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,13 +1,10 @@
 export const version = "0.1.0";
 
-export { DateTime } from "https://esm.sh/luxon@3.3.0";
-export {
-  encode as base64Encode,
-  // decode as base64Decode,
-} from "https://deno.land/std@0.192.0/encoding/base64.ts";
+export { DateTime } from "https://esm.sh/luxon@3.4.0";
+export { encode as base64Encode } from "https://deno.land/std@0.198.0/encoding/base64.ts";
 
 export {
   Application,
   Router,
   Response,
-} from "https://deno.land/x/oak@v12.5.0/mod.ts";
+} from "https://deno.land/x/oak@v12.6.0/mod.ts";
